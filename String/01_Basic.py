116d831ce88b32d1b498a49a097a5a8814f0ea0a
unknown <abdullah15-7683@diu.edu.bd>
2025-12-06 22:48:19 +0930

    String Methods

---
+++ String/01_Basic.py
@@ -0,0 +1,24 @@
+
+num_List = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+print(num_List[:])
+print(num_List[2:5])
+print(num_List[3:])
+print(num_List[:6])
+print(num_List[0:7:2])
+print(num_List[0:7:3])
+
+
+chai = "lemon,sugar,Ginger,Masala,Carrot,cardamom"
+chai2 = "lemon2, sugar2, Ginger2, Masala2, Carrot2, cardamom2"
+
+coffee = "------------Milk"
+
+print(chai.lower())
+print(chai.upper())
+
+print(coffee.strip('-'))
+
+print(chai.replace("Masala", "Honey"));
+
+print(chai2.split(" "));
+print(chai2.split(", "));
