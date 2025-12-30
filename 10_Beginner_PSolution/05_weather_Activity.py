00c8f034fa7171c55b063d645c97e32b542065bd
Foysalmeazi <abdullah15-7683@diu.edu.bd>
2025-12-30 09:43:26 +0930

    Weather Activity

---
+++ 10_Beginner_PSolution/05_weather_Activity.py
@@ -0,0 +1,13 @@
+
+weather = input('Enter the todays weather:').lower()
+
+if(weather=='sunny'):
+    print("Go for walking")
+elif(weather=='rainy'):
+    print("Read a good Book")
+elif(weather=='snowy'):
+    print("Build a snowman")
+elif(weather=='windy'):
+    print("Fly a Kite")
+else:
+    print("Do what you want")
