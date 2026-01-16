850212d2660f7330507748a375e127a74bd9944a
Foysalmeazi <abdullah15-7683@diu.edu.bd>
2026-01-16 11:02:59 +0930

    Multiplication of 2 numbers

---
+++ 10_problems_function/01_multiplication.py
@@ -0,0 +1,10 @@
+
+num1 = int(input('Enter num 1:'))
+num2 = int(input('Enter num 2:'))
+
+def multiplication(num1,num2):
+    result =  num1*num2;
+    print(f'{num1} * {num2} = {result}')
+
+multiplication(num1,num2)
+
